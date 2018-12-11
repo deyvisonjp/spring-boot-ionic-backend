dbddfac5c02b698b49f2610c4882d38b85ecc753
deyvisonjp <deyvisonjp@id.uff.br>
2018-12-11 21:43:00 -0200

    Testando a primeira classe dominio - Categoria

---
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -0,0 +1,68 @@
+package com.nelioalves.cursomc.domain;
+
+import java.io.Serializable;
+
+//Serializable -> Interface que garante que a classe possa ser convertida em bytes, 
+				//isso serve para que os objetos sejam gravados em arquivos (Exigencia linguagem java)
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L; //Ligado ao Serializable, indicando que é a primeira versão da classe padrão 1
+	
+	private Integer id;
+	private String nome;
+	
+	public Categoria() {
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	//HashCode -> Serve para comparar objetos por conteudo e não por ponteiros
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+
+	
+	
+	
+}
