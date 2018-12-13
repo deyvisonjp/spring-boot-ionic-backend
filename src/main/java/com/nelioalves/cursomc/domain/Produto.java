2704087b9a1401f3b8b16a6a6a0b5921cbaaa7b2
deyvisonjp <deyvisonjp@id.uff.br>
2018-12-13 17:38:53 -0200

    Produto e associacao muitos para muitos

--- src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -8,32 +8,35 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-/*
-*Serializable -> Interface que garante que a classe possa ser convertida em bytes, 
-*isso serve para que os objetos sejam gravados em arquivos (Exigencia linguagem java)
-*/
-
 @Entity
-public class Categoria implements Serializable {
-	private static final long serialVersionUID = 1L; //Ligado ao Serializable, indicando que é a primeira versão da classe padrão 1
+public class Produto implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //IDENTITY -> Id geração de id automatico
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 	
-	@ManyToMany(mappedBy="categorias") //Mapeia com a instancias categorias dentro de Produto
-	private List<Produto> produtos = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+			joinColumns = @JoinColumn(name = "produto_id"),
+			inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	public Categoria() {
+	public Produto() {
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -51,13 +54,21 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	public List<Produto> getProdutos() {
-		return produtos;
+
+	public Double getPreco() {
+		return preco;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	@Override
@@ -68,7 +79,6 @@ public class Categoria implements Serializable {
 		return result;
 	}
 
-	//HashCode -> Serve para comparar objetos por conteudo e não por ponteiros
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -77,7 +87,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -85,5 +95,7 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
+	
+	
 	
 }
